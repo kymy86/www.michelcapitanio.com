56d22482eba9476aa09af71d5f02645474608eae
Michel Capitanio <michel.capitanio@it.clara.net>
2021-09-25 13:21:54 +0200

    Updated cloufront tls protocol

--- terraform/michelcapitanio.tf
+++ terraform/michelcapitanio.tf
@@ -73,7 +73,7 @@ resource "aws_cloudfront_distribution" "website_distribution" {
 
   viewer_certificate {
     acm_certificate_arn      = aws_acm_certificate_validation.website_cert.certificate_arn
-    minimum_protocol_version = "TLSv1.1_2016"
+    minimum_protocol_version = "TLSv1.2_2021"
     ssl_support_method       = "sni-only"
   }
 }
